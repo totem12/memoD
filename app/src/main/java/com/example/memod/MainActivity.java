5a7b8ed18274dc7a660a2f88d031dae57729dde2
totem12 <7s1fma@gmail.com>
2020-07-04 16:46:06 +0900

    メモの削除を関数化した

--- app/src/main/java/com/example/memod/MainActivity.java
+++ app/src/main/java/com/example/memod/MainActivity.java
@@ -52,18 +52,8 @@ public class MainActivity extends AppCompatActivity {
         listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-                String idStr = adapter.getUuid(position);
 
-                SQLiteDatabase db = helper.getWritableDatabase();
-                try {
-                    db.execSQL("DELETE FROM MEMO_TABLE WHERE uuid = '" + idStr + "'");
-                    db.execSQL("DELETE FROM DATE_TABLE WHERE uuid = '" + idStr + "'");
-                } finally {
-                    db.close();
-                }
-
-                adapter.remove(position);
-                Toast.makeText(MainActivity.this, "削除しました", Toast.LENGTH_SHORT).show();
+                deleteMemo(position);
 
                 return true;
             }
@@ -140,6 +130,22 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(MainActivity.this, CreatePage.class);
         intent.putExtra("id", id);
         startActivity(intent);
+    }
+
+    //削除
+    private void deleteMemo(int position){
+        String idStr = adapter.getUuid(position);
+
+        SQLiteDatabase db = helper.getWritableDatabase();
+        try {
+            db.execSQL("DELETE FROM MEMO_TABLE WHERE uuid = '" + idStr + "'");
+            db.execSQL("DELETE FROM DATE_TABLE WHERE uuid = '" + idStr + "'");
+        } finally {
+            db.close();
+        }
+
+        adapter.remove(position);
+        Toast.makeText(MainActivity.this, "削除しました", Toast.LENGTH_SHORT).show();
     }
 
 }
